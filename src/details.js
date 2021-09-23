475cc26e52a593e77d50423b2c7ba8874067196a
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2021-09-23 20:06:44 +0200

    created classes for details and tasks

---
+++ src/details.js
@@ -0,0 +1,7 @@
+const details = [];
+class Detail {
+    constructor (details) {
+        this.details = details;
+    }
+};
+export {Detail, details};
---
+++ src/task.js
@@ -0,0 +1,13 @@
+import {Detail, details} from './details'
+
+class Task {
+    constructor(taskName, dueDate, ) {
+        this.taskName = taskName;
+        this.dueDate = dueDate;
+    }
+    removeTask() {
+    //find this task from tasks array;
+    }
+}
+
+export {Task};
