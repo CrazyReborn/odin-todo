f0b0bfc2317c30324bec5d064dbc6497913f2d35
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2021-09-29 20:31:57 +0200

    Now saves projects in local storage.

--- src/localstorage-function.js
+++ src/localstorage-function.js
@@ -1,4 +1,5 @@
 import { ProjectManager, Project, Task } from "./projects";
+import { checkTimeLeft } from "./check-time-left";
 
 
 const storage = (() => {
@@ -8,22 +9,31 @@ const storage = (() => {
         ProjectManager.projectList.forEach(project => {
             projects.push(JSON.stringify(project));
         })
+        localStorage.setItem(`projects`, `${projects}`);
+        console.log(`projects array: ${projects}`);
     }
+
+    /*
+    const load = () => {
+        const a = localStorage.getItem(`projects`);
+        projects.push = JSON.parse(a);
+        }
+        */
     return {
         projects,
         update,
+        load,
     }
 })();
 
 const checkStorage = () => {
-    if (storage.projects) {
+    if (localStorage.length == 0) {
         const defaultProject = Project('Buy PS5', 'Add 100 dollars to a save account each month', '2022-11-11', '1');
         ProjectManager.addProject(defaultProject);
         const defaulTask = Task('Create Account', 'Create a saving accout', '2021-11-11', '1');
         defaultProject.addTask(defaulTask);
     }
     else {
-        return false;
     }
 };
 
